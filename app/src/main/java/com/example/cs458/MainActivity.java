e5446d296164681191dd1a2abc67219badce53db
İsmet Alp Eren <alp.eren@ug.bilkent.edu.tr>
2022-03-22 00:36:16 +0300

    second page

--- app/src/main/java/com/example/cs458/MainActivity.java
+++ app/src/main/java/com/example/cs458/MainActivity.java
@@ -2,8 +2,7 @@ package com.example.cs458;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
-import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -12,10 +11,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import java.util.Calendar;
 
@@ -31,8 +28,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     EditText otherVaccine;
     CheckBox checkBox;
     Button submit;
-    private DatePickerDialog datePickerDialog;
-    private Button dateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,12 +57,28 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         name.addTextChangedListener(this);
         city.addTextChangedListener(this);
         ase.addTextChangedListener(this);
+        otherVaccine.addTextChangedListener(this);
 
-        initDatePicker();
-        dateButton = findViewById(R.id.datePickerButton);
-        System.out.println(dateButton.getText().toString());
-
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSecond();
+            }
+        });
     };
+
+    public void openSecond(){
+        Intent intent = new Intent(this, SecondPage.class);
+        intent.putExtra("name",name.getText().toString());
+        intent.putExtra("city",city.getText().toString());
+        intent.putExtra("ase",ase.getText().toString());
+        intent.putExtra("otherVaccine",otherVaccine.getText().toString());
+        intent.putExtra("gender",gender.getSelectedItem().toString());
+        intent.putExtra("vtype",vtype.getSelectedItem().toString());
+        intent.putExtra("check",String.valueOf(checkBox.isChecked()));
+
+        startActivity(intent);
+    }
 
 
     @Override
@@ -75,8 +86,33 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         System.out.println(vaccines[i].equals("Other"));
         if(vaccines[i].equals("Other")) {
             otherVaccine.setVisibility(View.VISIBLE);
+            if(otherVaccine.getVisibility() == View.VISIBLE && otherVaccine.getText().toString().isEmpty()){
+                submit.setVisibility(View.INVISIBLE);
+            }
         } else {
             otherVaccine.setVisibility(View.GONE);
+            boolean check = false;
+            if(name.getText().toString().isEmpty()){
+                check = false;
+            }
+            else if(city.getText().toString().isEmpty()){
+                check = false;
+            }
+            else if(ase.getText().toString().isEmpty()){
+                check = false;
+            }
+            else if(otherVaccine.getVisibility() == View.VISIBLE && otherVaccine.getText().toString().isEmpty()){
+                check = false;
+            }
+            else {
+                check = true;
+            }
+
+            if(check){
+                submit.setVisibility(View.VISIBLE);
+            } else {
+                submit.setVisibility(View.INVISIBLE);
+            }
         }
     }
 
@@ -97,13 +133,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         if(name.getText().toString().isEmpty()){
             check = false;
         }
-        else if(dateButton.getText().toString().equals("Birth date")){
-            check = false;
-        }
         else if(city.getText().toString().isEmpty()){
             check = false;
         }
         else if(ase.getText().toString().isEmpty()){
+            check = false;
+        }
+        else if(otherVaccine.getVisibility() == View.VISIBLE && otherVaccine.getText().toString().isEmpty()){
             check = false;
         }
         else {
@@ -122,99 +158,5 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 
-    private String getTodaysDate()
-    {
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        month = month + 1;
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-        return makeDateString(day, month, year);
-    }
 
-    private void initDatePicker()
-    {
-        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
-        {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day)
-            {
-                month = month + 1;
-                String date = makeDateString(day, month, year);
-                dateButton.setText(date);
-
-                boolean check = false;
-                if(name.getText().toString().isEmpty()){
-                    check = false;
-                }
-                else if(dateButton.getText().toString().equals("Birth date")){
-                    check = false;
-                }
-                else if(city.getText().toString().isEmpty()){
-                    check = false;
-                }
-                else if(ase.getText().toString().isEmpty()){
-                    check = false;
-                }
-                else {
-                    check = true;
-                }
-
-                if(check){
-                    submit.setVisibility(View.VISIBLE);
-                } else {
-                    submit.setVisibility(View.INVISIBLE);
-                }
-            }
-        };
-
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-
-        int style = AlertDialog.THEME_HOLO_LIGHT;
-
-        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
-    }
-
-    private String makeDateString(int day, int month, int year)
-    {
-        return getMonthFormat(month) + " " + day + " " + year;
-    }
-
-    private String getMonthFormat(int month)
-    {
-        if(month == 1)
-            return "JAN";
-        if(month == 2)
-            return "FEB";
-        if(month == 3)
-            return "MAR";
-        if(month == 4)
-            return "APR";
-        if(month == 5)
-            return "MAY";
-        if(month == 6)
-            return "JUN";
-        if(month == 7)
-            return "JUL";
-        if(month == 8)
-            return "AUG";
-        if(month == 9)
-            return "SEP";
-        if(month == 10)
-            return "OCT";
-        if(month == 11)
-            return "NOV";
-        if(month == 12)
-            return "DEC";
-
-        return "JAN";
-    }
-
-    public void openDatePicker(View view)
-    {
-        datePickerDialog.show();
-    }
 }
---
+++ app/src/main/java/com/example/cs458/SecondPage.java
@@ -0,0 +1,51 @@
+package com.example.cs458;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class SecondPage extends AppCompatActivity {
+
+    TextView name;
+    TextView city;
+    TextView gender;
+    TextView vtype;
+    TextView ase;
+    TextView otherVaccine;
+    TextView checkBox;
+    LinearLayout main;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_page);
+        main = findViewById(R.id.main);
+
+        name = findViewById(R.id.name);
+        city = findViewById(R.id.city);
+        gender = findViewById(R.id.gender);
+        vtype = findViewById(R.id.vtype);
+        ase = findViewById(R.id.ase);
+        otherVaccine = findViewById(R.id.otherVaccine);
+        checkBox = findViewById(R.id.checkBox);
+
+        Intent intent = getIntent();
+
+        name.setText("Name is: "+ intent.getStringExtra("name"));
+        city.setText("City is: "+ intent.getStringExtra("city"));
+        gender.setText("Gender is: "+ intent.getStringExtra("gender"));
+        vtype.setText("Vaccination Type is: "+ intent.getStringExtra("vtype"));
+        ase.setText("Side Effects are: "+ intent.getStringExtra("ase"));
+        otherVaccine.setText("VT is: "+ intent.getStringExtra("otherVaccine"));
+        checkBox.setText("AS is: "+ intent.getStringExtra("checkBox"));
+
+
+    }
+}
