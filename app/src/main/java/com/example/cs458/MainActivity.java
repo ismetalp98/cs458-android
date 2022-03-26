0432c9b9e130d1a7ef214b161cd40da14fceb49f
İsmet Alp Eren <alp.eren@ug.bilkent.edu.tr>
2022-03-26 23:05:34 +0300

    final

--- app/src/main/java/com/example/cs458/MainActivity.java
+++ app/src/main/java/com/example/cs458/MainActivity.java
@@ -29,10 +29,10 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, Radi
     EditText dd, mm, yyyy;
     EditText city, ase;
     EditText otherVaccine;
-    RadioButton radioYes, radioNo;
-    RadioGroup groupGender, groupVaccination;
+    RadioGroup groupGender, groupVaccination, groupPositive;
     CheckBox checkase;
     Button submit;
+    String genderSelected, vaccinationSelected, positiveSelected;
     String cities[] = {"adana", "adıyaman", "afyon", "ağrı", "amasya", "ankara", "antalya", "artvin",
             "aydın", "balıkesir", "bilecik", "bingöl", "bitlis", "bolu", "burdur", "bursa", "çanakkale",
             "çankırı", "çorum", "denizli", "diyarbakır", "edirne", "elazığ", "erzincan", "erzurum", "eskişehir",
@@ -48,21 +48,18 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, Radi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        boolean[] arr = new boolean[5];
         name = findViewById(R.id.name);
-        city = findViewById(R.id.city);
-
-        ase = findViewById(R.id.ase);
-        radioYes = findViewById(R.id.radioYes);
-        radioNo = findViewById(R.id.radioNo);
-        submit = findViewById(R.id.submit);
-        otherVaccine = findViewById(R.id.otherVaccine);
         dd = findViewById(R.id.dd);
         mm = findViewById(R.id.mm);
         yyyy = findViewById(R.id.yyyy);
-        checkase = findViewById(R.id.checkase);
+        city = findViewById(R.id.city);
         groupGender = findViewById(R.id.groupGender);
         groupVaccination = findViewById(R.id.groupVaccination);
+        otherVaccine = findViewById(R.id.otherVaccine);
+        checkase = findViewById(R.id.checkase);
+        ase = findViewById(R.id.ase);
+        groupPositive = findViewById(R.id.groupPositive);
+        submit = findViewById(R.id.submit);
 
 
         name.addTextChangedListener(this);
@@ -75,6 +72,7 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, Radi
         groupGender.setOnCheckedChangeListener(this);
         groupVaccination.setOnCheckedChangeListener(this);
         checkase.setOnCheckedChangeListener(this);
+        groupPositive.setOnCheckedChangeListener(this);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,27 +80,36 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, Radi
                 openSecond();
             }
         });
-    };
+    }
+
+    ;
 
     public void openSecond() {
         Intent intent = new Intent(this, SecondPage.class);
         intent.putExtra("name", name.getText().toString());
+        intent.putExtra("bdate", dd.getText().toString() + "-" + mm.getText().toString() + "-" + yyyy.getText().toString());
         intent.putExtra("city", city.getText().toString());
-        intent.putExtra("ase", ase.getText().toString());
-        intent.putExtra("otherVaccine", otherVaccine.getText().toString());
-        intent.putExtra("check", String.valueOf(radioYes.isChecked()));
+        intent.putExtra("gender", genderSelected);
+        intent.putExtra("vaccination", vaccinationSelected.equals("Other") ? otherVaccine.getText().toString() : vaccinationSelected);
+        intent.putExtra("ase", checkase.isChecked() ? ase.getText().toString() : "No side effect");
+        intent.putExtra("after", positiveSelected);
         startActivity(intent);
     }
 
     @Override
     public void onCheckedChanged(RadioGroup radioGroup, int i) {
         RadioButton checkedRadioButton = radioGroup.findViewById(i);
-        if(getResources().getResourceEntryName(radioGroup.getId()).equals("groupVaccination")){
-            if(checkedRadioButton.getText().toString().equals("Other"))
+        if (getResources().getResourceEntryName(radioGroup.getId()).equals("groupVaccination")) {
+            vaccinationSelected = checkedRadioButton.getText().toString();
+            if (checkedRadioButton.getText().toString().equals("Other"))
                 otherVaccine.setVisibility(View.VISIBLE);
             else {
                 otherVaccine.setVisibility(View.GONE);
             }
+        } else if (getResources().getResourceEntryName(radioGroup.getId()).equals("groupGender")) {
+            genderSelected = checkedRadioButton.getText().toString();
+        } else {
+            positiveSelected = checkedRadioButton.getText().toString();
         }
         if (checkEmpty()) {
             if (checkFields())
@@ -114,10 +121,9 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, Radi
 
     @Override
     public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-        if(b){
+        if (b) {
             ase.setVisibility(View.VISIBLE);
-        }
-        else{
+        } else {
             ase.setVisibility(View.GONE);
         }
         if (checkEmpty()) {
@@ -158,16 +164,17 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, Radi
             return false;
         else if (yyyy.getText().toString().isEmpty())
             return false;
-        else if(groupGender.getCheckedRadioButtonId() == -1)
+        else if (groupGender.getCheckedRadioButtonId() == -1)
             return false;
-        else if(groupVaccination.getCheckedRadioButtonId() == -1)
+        else if (groupVaccination.getCheckedRadioButtonId() == -1)
+            return false;
+        else if (groupPositive.getCheckedRadioButtonId() == -1)
             return false;
         return true;
     }
 
     public boolean checkFields() {
         int check = 0;
-        int maxDay = checkDay(Integer.valueOf(mm.getText().toString()));
 
         if (name.getText().toString().matches(".*[0-9].*")) {
             name.setError("No number");
@@ -180,27 +187,47 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, Radi
         if (mm.getText().toString().matches(".*[a-zA-Z].*")) {
             mm.setError("No letter");
             check = 1;
-        } else if (Integer.valueOf(mm.getText().toString()) > 12 || Integer.valueOf(mm.getText().toString()) < 1) {
-            mm.setError("Check boundaries");
-            check = 1;
+        } else {
+            if (Integer.valueOf(mm.getText().toString()) > 12 || Integer.valueOf(mm.getText().toString()) < 1) {
+                mm.setError("Check boundaries");
+                check = 1;
+            }
         }
         if (dd.getText().toString().matches(".*[a-zA-Z].*")) {
             dd.setError("No letter");
             check = 1;
-        } else if (Integer.valueOf(dd.getText().toString()) > maxDay || Integer.valueOf(dd.getText().toString()) < 1) {
-            dd.setError("Check boundaries");
-            check = 1;
+        } else {
+            if (!mm.getText().toString().matches(".*[a-zA-Z].*") && Integer.valueOf(dd.getText().toString()) > checkDay(Integer.valueOf(mm.getText().toString())) || Integer.valueOf(dd.getText().toString()) < 1) {
+                dd.setError("Check boundaries");
+                check = 1;
+            }
         }
         if (yyyy.getText().toString().matches(".*[a-zA-Z].*")) {
             yyyy.setError("No letter");
             check = 1;
-        } else if (Integer.valueOf(yyyy.getText().toString()) > 2022 || Integer.valueOf(yyyy.getText().toString()) < 1900) {
-            yyyy.setError("Check boundaries");
-            check = 1;
+        } else {
+            if (Integer.valueOf(yyyy.getText().toString()) > 2022 || Integer.valueOf(yyyy.getText().toString()) < 1900) {
+                yyyy.setError("Check boundaries");
+                check = 1;
+            }
         }
         if (check == 1)
             return false;
-        return true;
+        else {
+            Calendar c = Calendar.getInstance();
+            Calendar now = Calendar.getInstance();
+            c.set(Integer.valueOf(yyyy.getText().toString()), Integer.valueOf(mm.getText().toString()) - 1, Integer.valueOf(dd.getText().toString()), 0, 0);
+            if (c.after(now)) {
+                mm.setError("Check boundaries");
+                dd.setError("Check boundaries");
+                yyyy.setError("Check boundaries");
+                return false;
+            }
+            dd.setError(null);
+            mm.setError(null);
+            yyyy.setError(null);
+            return true;
+        }
     }
 
     @Override
@@ -211,7 +238,7 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, Radi
     public int checkDay(int mm) {
         int otuz[] = {4, 6, 9, 11};
         if (mm == 2) {
-            if (Integer.valueOf(yyyy.getText().toString()) % 4 == 0) {
+            if (!yyyy.getText().toString().matches(".*[a-zA-Z].*") && Integer.valueOf(yyyy.getText().toString()) % 4 == 0) {
                 return 29;
             }
             return 28;
--- app/src/main/java/com/example/cs458/SecondPage.java
+++ app/src/main/java/com/example/cs458/SecondPage.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -15,41 +16,43 @@ import android.widget.TextView;
 public class SecondPage extends AppCompatActivity {
 
     TextView name;
+    TextView bdate;
     TextView city;
     TextView gender;
-    TextView vtype;
+    TextView vaccination;
     TextView ase;
-    TextView otherVaccine;
-    TextView checkBox;
-    LinearLayout main;
+    TextView after;
+    Button edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_page);
-        main = findViewById(R.id.main);
 
         name = findViewById(R.id.name);
+        bdate = findViewById(R.id.bdate);
         city = findViewById(R.id.city);
         gender = findViewById(R.id.gender);
-        vtype = findViewById(R.id.vtype);
+        vaccination = findViewById(R.id.vaccination);
         ase = findViewById(R.id.ase);
-        otherVaccine = findViewById(R.id.otherVaccine);
-        checkBox = findViewById(R.id.checkBox);
+        after = findViewById(R.id.after);
+        edit = findViewById(R.id.edit);
 
         Intent intent = getIntent();
 
         name.setText("Name is: "+ intent.getStringExtra("name"));
+        bdate.setText("Birth date is: " + intent.getStringExtra("bdate"));
         city.setText("City is: "+ intent.getStringExtra("city"));
         gender.setText("Gender is: "+ intent.getStringExtra("gender"));
-        vtype.setText("Vaccination Type is: "+ intent.getStringExtra("vtype"));
+        vaccination.setText("Vaccination is: "+ intent.getStringExtra("vaccination"));
         ase.setText("Side Effects are: "+ intent.getStringExtra("ase"));
-        if(intent.getStringExtra("otherVaccine").length() > 0)
-            otherVaccine.setText("VT is: "+ intent.getStringExtra("otherVaccine"));
-        else
-            otherVaccine.setVisibility(View.GONE);
-        checkBox.setText("AS is: "+ intent.getStringExtra("checkBox"));
+        after.setText("After 3th: " + intent.getStringExtra("after"));
 
-
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
